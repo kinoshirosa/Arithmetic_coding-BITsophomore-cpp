ab2bc70693350aa7acdbecc89b9f850934e02f6f
HerbLee <3113440157@qq.com>
2025-04-12 10:23:27 +0800

    add get_sorted_symbol_list head and source

--- get_sorted_symbol_list.h
+++ get_sorted_symbol_list.h
@@ -4,4 +4,5 @@
 
 ListNode<Symbol> find_maxfreq(SymbolList&);
 ListNode<Symbol> find_minfreq(SymbolList& symbol_list);
+void print_symbol_list(SymbolList& sorted_symbol_list);
 void get_sorted_symbol_list(std::ifstream& coding_file, SymbolList& target_symbol_list);
