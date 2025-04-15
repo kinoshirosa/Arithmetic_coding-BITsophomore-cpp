cd72480b226028ad8cd27a1940eda06bdf2cd240
HerbLee <3113440157@qq.com>
2025-04-15 10:34:36 +0800

    set function into head file

--- get_sorted_symbol_list.cpp
+++ get_sorted_symbol_list.cpp
@@ -73,3 +73,54 @@ void print_symbol_list(SymbolList& sorted_symbol_list) {
 		pcurrent_symbol_node = pcurrent_symbol_node->next;
 	}
 }
+
+double get_arith_code(std::ifstream& coding_file, SymbolList sorted_symbol_list) {
+	double total_num = sorted_symbol_list.Find_index(0)->data.get_num();
+	double upper_lim = 1;
+	double lower_lim = 0;
+	double upper_percent = 1;
+	double lower_percent = 0;
+	double ul_length = upper_lim - lower_lim;
+	unsigned int accumulated_number = 0;
+	SymbolList sorted_symbol_coord;
+	ListNode<Symbol>* p = sorted_symbol_list.GetHead()->next;
+	Symbol current_symbol;
+	char new_char;
+
+	while (p != nullptr) {
+		current_symbol.set_num(accumulated_number);
+		current_symbol.set_sym(p->get_data().get_sym());
+		sorted_symbol_coord.Insert(current_symbol, sorted_symbol_coord.GetLength());
+		accumulated_number += p->data.get_num();
+		p = p->next;
+		// we add the number to acc_num after the Insert
+		// because the LinkList is just a foreward-only list
+		// we can only get the numbers after the node, so we must get lower_percent from current node
+		// then get upper_percent from next node
+		// when is comes to the most frequent letter 'e' for example
+		// the acc_num of 'e' is 0 but its own frequncy, bucause 0 is its lower_percent
+	}
+	if (!coding_file.is_open()) {
+		std::cout << "file open failure!" << std::endl;
+		return -1;
+	}
+	else {
+		while (!coding_file.eof()) {
+			coding_file >> new_char;		// read one char each time for coding
+			current_symbol.set_sym(new_char);
+			p = sorted_symbol_coord.Find_value(current_symbol);
+			lower_percent = p->data.get_num() / total_num;
+			if (p->next == nullptr) {
+				upper_percent = 1;
+			}
+			else {
+				upper_percent = p->next->data.get_num() / total_num;
+			}
+			upper_lim = lower_lim + ul_length * upper_percent;
+			lower_lim = lower_lim + ul_length * lower_percent;
+			ul_length = upper_lim - lower_lim;
+		}
+	}
+	return (upper_lim + lower_lim) / 2;
+}
+
--- get_sorted_symbol_list.h
+++ get_sorted_symbol_list.h
@@ -6,3 +6,4 @@ ListNode<Symbol> find_maxfreq(SymbolList&);
 ListNode<Symbol> find_minfreq(SymbolList& symbol_list);
 void print_symbol_list(SymbolList& sorted_symbol_list);
 void get_sorted_symbol_list(std::ifstream& coding_file, SymbolList& target_symbol_list);
+double get_arith_code(std::ifstream& coding_file, SymbolList sorted_symbol_list);
